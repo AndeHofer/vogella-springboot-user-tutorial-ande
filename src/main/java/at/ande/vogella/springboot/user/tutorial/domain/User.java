502fd5611811515d4cf9bf4e261bfd1c6230e8f5
AndeHofer <andehofer@gmail.com>
2022-01-18 10:11:33 +0100

    initial commit

---
+++ src/main/java/at/ande/vogella/springboot/user/tutorial/domain/User.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package at.ande.vogella.springboot.user.tutorial.domain;
+
+/**
+ * 
+ * @author vhhsa02
+ * @since (January 2022)
+ */
+public class User {
+
+}
---
+++ src/test/java/at/ande/vogella/springboot/user/tutorial/ApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ande.vogella.springboot.user.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
